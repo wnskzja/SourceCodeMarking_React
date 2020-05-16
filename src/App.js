b2f7ccce6ea1757a935003b62575a34b3908ce83
1653021 <1653021@student.hcmus.edu.vn>
2020-05-16 15:06:58 +0700

    Add router.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import React from "react";
-import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 import SignIn from "./components/SignIn/SignIn";
-function App() {
+
+export default function App() {
   return (
-    <div className="App">
-      <SignIn />
-    </div>
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <SignIn />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
