6dfcec21d5ed1c810090a459919f6e02ad21a392
CuShi <1653014@student.hcmus.edu.vn>
2020-05-16 15:05:15 +0700

    design sign in sign up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import SignIn from "./components/SignIn/SignIn";
+import SignUp from "./components/SignUp/SignUp";
+
 function App() {
   return (
     <div className="App">
-      <SignIn />
+      <SignUp />
     </div>
   );
 }
